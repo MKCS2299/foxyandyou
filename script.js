677f896f41f1a519a9dcf02b15f3e80d69f08aea
MKCS2299 <98272921+MKCS2299@users.noreply.github.com>
2026-02-07 13:00:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+let step = 0;
+
+const calendar = document.getElementById("calendar");
+const fox = document.getElementById("fox");
+const text = document.getElementById("text");
+const light = document.getElementById("light");
+
+const months = [
+  "January",
+  "April",
+  "August"
+];
+
+const texts = [
+  "Some things don’t need to be said.",
+  "They’re noticed by staying.",
+  ""
+];
+
+function showText(content) {
+  text.classList.remove("show");
+  setTimeout(() => {
+    text.innerHTML = content;
+    if (content) text.classList.add("show");
+  }, 300);
+}
+
+function nextStep() {
+  if (step === 0) {
+    calendar.innerText = months[0];
+    showText(texts[0]);
+    step++;
+  }
+  else if (step === 1) {
+    calendar.innerText = months[1];
+    showText(texts[1]);
+    step++;
+  }
+  else if (step === 2) {
+    calendar.innerText = months[2];
+    text.classList.remove("show");
+    step++;
+  }
+  else if (step === 3) {
+    light.classList.add("show");
+    fox.classList.remove("waiting");
+    fox.classList.add("happy");
+    showText("I’m still here.");
+    step++;
+  }
+}
+
+// initial
+calendar.innerText = months[0];
+showText(texts[0]);
